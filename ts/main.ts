36c28cc94771f742085442fe2247e52d8f5fbd54
Patrick Walsh <patrick.walsh@ironcorelabs.com>
2015-08-25 19:08:37 -0600

    Changed phone number to generic

--- ts/main.ts
+++ ts/main.ts
@@ -1,10 +1,11 @@
 /// <reference path="refs/jquery/jquery.d.ts" />
 /// <reference path="refs/stickyfill.d.ts" />
+/// <reference path="refs/waypoint.d.ts" />
 
-declare var disqus_shortname: string; 
-declare var disqus_identifier: string; 
-declare var disqus_title: string; 
-declare var disqus_url: string; 
+declare var disqus_shortname: string;
+declare var disqus_identifier: string;
+declare var disqus_title: string;
+declare var disqus_url: string;
 
 
 // Redirect if someone access github.io directly
@@ -28,50 +29,65 @@ switch(hostname) {
       break;
 }
 
+// Break out of any wrapping frames
+if (top.location != location) {
+    top.location.href = document.location.href ;
+
 // After load...
 $(function() {
   $('nav').Stickyfill();
 
   var form:HTMLFormElement = <HTMLFormElement>document.getElementById('mc-embedded-subscribe-form');
-  
+
   form.reset = function() {
     // override the reset function -- its the only way we know
     // mailchimp had success submitting the form.
     $('#mc-embedded-subscribe-form .input-group').hide(1000);
   }
-  
+
   // De-obfuscate email address
   var email = $('#contactemail').text() + '@' + window.location.hostname;
   $('#contactemail').text(email);
   $('#contactemail').attr('href', 'mailto:'+email);
-  
+
   // De-obfuscate phone number
 // to obfuscate, start with a number and do this:
   // "123-456-789".split('').map(function(v, idx) { return v.charCodeAt(0) ^ idx; }).join("-");
-  var phone = $("#contacttel").text().split('-').map(function(v, idx) { 
-    return String.fromCharCode(parseInt(v, 10) ^ idx); 
+  var phone = $("#contacttel").text().split('-').map(function(v, idx) {
+    return String.fromCharCode(parseInt(v, 10) ^ idx);
   }).join('');
   $('#contacttel').attr('itemprop', 'telephone')
     .attr('href', "tel:+1"+phone.replace(/-/g,''))
     .text(phone);
 
+  // $('.stripe-thumbnail img' ).waypoint( {
+    // offset: '75%',
+    // handler: function() {
+      // console.log("waypoint!");
+      // console.log($(this));
+      // if (this.element) {
+        // $(this.element).addClass('img-animated');
+      // }
+    // }
+  // });
+
   if ($('#disqus_thread').length && disqus_shortname) {
-    var dsq = document.createElement('script'); 
-    dsq.type = 'text/javascript'; 
+    var dsq = document.createElement('script');
+    dsq.type = 'text/javascript';
     dsq.async = true;
     dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
     (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
   }
-  
+
   if ($('#twitter-container').length) {
-    var tw = document.createElement('script'); 
-    tw.type = 'text/javascript'; 
+    var tw = document.createElement('script');
+    tw.type = 'text/javascript';
     tw.async = true;
     tw.id = "twitter-wjs";
     tw.src = '//platform.twitter.com/widgets.js';
     (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(tw);
   }
-  
+
 
   function poormanStickyfill() {
     var header = document.getElementById('header');
@@ -83,5 +99,5 @@ $(function() {
     }
     document.addEventListener('scroll', onScroll);
   }
- 
+
 });
